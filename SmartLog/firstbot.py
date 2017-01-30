f7df0c202971567d33af6f76a3a09dc36baa4a1e
junemp <junemp@gmail.com>
2017-01-30 22:20:14 +0900

    * upload

--- SmartLog/firstbot.py
+++ SmartLog/firstbot.py
@@ -1,51 +1,112 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+from telegram import ReplyKeyboardMarkup
 import logging
 import json
-
-updater =   Updater(token='249362685:AAF-pOpNK2yQ22JciNF0MO_7GcXJlZ3e234')
-dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-CMD_START = '/start'
-CMD_STOP = '/stop'
-CMD_HELP = '/help'
-CMD_BROADCAST = '/broadcast'
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
 
-CUSTOM_KEYBOARD = [
-    [CMD_START],
-    [CMD_STOP],
-    [CMD_HELP]
-]
+reply_keyboard = [['Age', 'Favourite colour'],
+                  ['Number of siblings', 'Something else...'],
+                  ['Done']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+def facts_to_str(user_data):
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append('%s - %s' % (key, value))
+
+    return "\n".join(facts).join(['\n', '\n'])
+
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="나는 Bot이다.")
+    update.message.reply_text(
+        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
+        "Why don't you tell me something about yourself?",
+        reply_markup=markup)
+    return CHOOSING
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def regular_choice(bot, update, user_data):
+    text = update.message.text
+    user_data['choice'] = text
+    update.message.reply_text('Your %s? Yes, I would love to hear about that!' % text.lower())
 
-def echo(bot, update):
-    reply_markup = json.dumps({
-        'keyboard': CUSTOM_KEYBOARD,
-        'resize_keyboard': True,
-        'one_time_keyboard': False,
-        'selective': True
-    })
-    logger.info("chat username=" + update.message.chat.username)
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text, reply_markup=reply_markup)
+    return TYPING_REPLY
 
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+def custom_choice(bot, update):
+    update.message.reply_text('Alright, please send me the category first, '
+                              'for example "Most impressive skill"')
 
-def unknown(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, 이해하지 못하겠군요")
+    return TYPING_CHOICE
 
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+def received_information(bot, update, user_data):
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
 
-updater.start_polling()
-updater.idle()
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "%s"
+                              "You can tell me more, or change your opinion on something."
+                              % facts_to_str(user_data),
+                              reply_markup=markup)
+
+    return CHOOSING
+
+def done(bot, update, user_data):
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text("I learned these facts about you:"
+                              "%s"
+                              "Until next time!" % facts_to_str(user_data))
+
+    user_data.clear()
+    return ConversationHandler.END
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def main():
+    updater = Updater(token='249362685:AAF-pOpNK2yQ22JciNF0MO_7GcXJlZ3e234')
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            CHOOSING: [RegexHandler('^(Age|Favourite colour|Number of siblings)$',
+                                    regular_choice,
+                                    pass_user_data=True),
+                       RegexHandler('^Something else...$',
+                                    custom_choice),
+                       ],
+            TYPING_CHOICE: [MessageHandler(Filters.text,
+                                           regular_choice,
+                                           pass_user_data=True),
+                            ],
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information,
+                                          pass_user_data=True),
+                           ],
+
+        },
+        fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
+
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
 
 
