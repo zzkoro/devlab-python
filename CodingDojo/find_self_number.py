d6f776dbc3dfcba189de96c1e4aa5c65c08f0065
junemp <junemp@gmail.com>
2017-01-30 18:12:56 +0900

    * upload

--- CodingDojo/find_self_number.py
+++ CodingDojo/find_self_number.py
@@ -1,7 +1,7 @@
 def func_dn(n):
     return n + sum([int(ni) for ni in str(n)])
 
-self_numbers = set(range(1,5000)) - set([func_dn(n) for n in range(1,5000)])
+self_numbers = set(range(1,5000)) - set(func_dn(n) for n in range(1,5000))
 
 print('sum of self numbers:', sum(self_numbers))
 
